cfd26ec2decd0d0cb530955017c21a32533b0c80
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2026-06-03 09:14:51 +0700

    Update About.php

--- 04 Controller/phpmvc/4runProgram3/app/controllers/About.php
+++ 04 Controller/phpmvc/4runProgram3/app/controllers/About.php
@@ -1,6 +1,6 @@
 <?php  
 
-	class Home {
+	class About {
 		public function index($string1 = 'Calm', $string2 = 'Humble', $string3 = 'Strong') {
 			// echo "Keep Calm, Stay Humble, Stay Strong";
 			echo "Keep $string1, Stay $string2, Stay $string3";
