5b63e1d6930a75a7c56bf871e28611061c9cad0f
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2026-06-10 15:50:05 +0700

    Add files via upload

---
+++ 09 Data Insert/phpmvc/2/app/controllers/Article.php
@@ -0,0 +1,32 @@
+<?php  
+
+	class Article extends Controller{
+		public function index() {
+			$data['title'] = 'Article Collection';
+			$data['article2'] = $this->model('Article_model')->getDataArticle();
+
+			$this->view('templates/header', $data);
+			$this->view('article/index', $data);
+			$this->view('templates/footer');
+		}
+
+		public function detail($id) {
+			$data['title'] = 'Article Collection';
+			$data['article2'] = $this->model('Article_model')->getDataArticleById($id);
+
+			$this->view('templates/header', $data);
+			$this->view('article/detail', $data);
+			$this->view('templates/footer');
+		}
+
+		public function add() {
+			// var_dump($_POST);
+
+			if( $this->model('Article_model')->addDataArticle($_POST) > 0 ) {
+				header('Location: ' . BASEURL . '/article');
+				exit;
+			}
+		}
+	}
+
+?>
